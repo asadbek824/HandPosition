afb804d5e2d8a5829fff71c42007d369228f016a
asadbek824 <yoldoshev5007@icloud.com>
2024-11-12 14:42:27 +0500

    Initial Commit

---
+++ HandPosition/HandPositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandPositionApp.swift
+//  HandPosition
+//
+//  Created by Asadbek Yoldoshev on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HandPositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
